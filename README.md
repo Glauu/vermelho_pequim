12bacd849987f5c157af462516e6715ff393de65
Glauu <gcelestinodasilva@gmail.com>
2025-09-18 09:17:51 -0300

    Iniciei o repositório atualizado.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vermelho Pequim
+Portfólio da banda
+
+Estudo sobre Fundamentos de HTML CSS Java Script
+
+
+Conheça o vermelho Pequim
+
+![](img/adaPintura.png)
+
---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+function validacampo(){
+ if() {
+  alert("Prontinho! Você receberá os dados por email.")
+ }else{
+  alert("Por favor preencha os dados.") 
+}}
+
+var botaoenviar = document.getElementById("botaoenviar")
+
+botaoenviar.addEventListener("click", validacampo)
+
+var nome = document.getElementById("nome").value
+var email = document.getElementById("email").value
+var = document.getElementById("telefone"). value
